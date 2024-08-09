e27d3ff6896a63011ba41fcb7198bc2bf2461315
cleiton viana <estudo.cleiton@gmail.com>
2024-08-09 08:26:57 -0300

    feature(contact): Check if email fields is empty

--- internal/domain/entitie/contact.go
+++ internal/domain/entitie/contact.go
@@ -1,5 +1,10 @@
 package entitie
 
+import (
+	"errors"
+	"strings"
+)
+
 type Contact struct {
 	Phone int
 	Email string
@@ -7,10 +12,21 @@ type Contact struct {
 
 func (c *Contact) CheckIfFieldsAreEmpty() *[]error {
 	var errs []error
+	err := c.checkIfEmailIsEmpyt()
+	if err != nil {
+		errs = append(errs, err)
+	}
 	// verify email address
 	// verify phone number
 	if len(errs) != 0 {
 		return &errs
+	}
+	return nil
+}
+
+func (c *Contact) checkIfEmailIsEmpyt() error {
+	if strings.Trim(c.Email, " ") == "" {
+		return errors.New("email field cannot is empty")
 	}
 	return nil
 }
