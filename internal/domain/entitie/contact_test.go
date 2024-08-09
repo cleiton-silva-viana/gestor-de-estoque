26d63467516b51bd0a28ba32bb975fdc881f71de
cleiton viana <estudo.cleiton@gmail.com>
2024-08-09 08:19:51 -0300

    test(contact): Create a checker for email empty

--- internal/domain/entitie/contact_test.go
+++ internal/domain/entitie/contact_test.go
@@ -6,12 +6,20 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var Corporation = Contact{
+var corporation = Contact{
 	Phone: 2140028922,
 	Email: "corporation@email.com",
 }
 
 func Test_CheckIfFieldsAreEmpyt_AllFieldsAreOk(t *testing.T) {
-	result := Corporation.CheckIfFieldsAreEmpty()
+	result := corporation.CheckIfFieldsAreEmpty()
 	assert.Nil(t, result, "The object can't have return error")
+}
+
+func Test_CheckIfFieldsAreEmpty_WithEmailEmpty(t *testing.T) {
+	corporation.Email = "                 "
+
+	result := corporation.CheckIfFieldsAreEmpty()
+
+	assert.NotNil(t, result, "Error expected - email address is empty")
 }
